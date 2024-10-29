10c3add502747f07265de43a7b3616b02723f84c
Harshit Joshi <harshit.joshi@infoneotech.com>
2024-10-29 16:03:32 +0530

    hua

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $('.carousel').slick({
       slidesToShow: 2,
       slidesToScroll: 1,
-      autoplay: true,
+    //   autoplay: true,
       autoplaySpeed: 2000,
       responsive: [
           {
